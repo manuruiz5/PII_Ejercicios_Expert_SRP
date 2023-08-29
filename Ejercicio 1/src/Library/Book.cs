6220f2a0b977c7b5e162c7e8fa01cbd1c07350ac
manuruiz5 <manuela.ruiz@correo.ucu.edu.uy>
2023-08-29 15:48:09 -0300

    Clases listas

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -10,13 +10,17 @@ namespace SRP
         public string Author { get ; }
         public string Code { get ;  }
         //public string LibrarySector { get ; set; } // No es responsabilidad del libro conocer esto
-       // public string LibraryShelve { get ; set; } 
+        //public string LibraryShelve { get ; set  } 
 
         public Book(String title, String author, String code)
         {
             this.Title = title;
             this.Author = author;
             this.Code = code;
+        }
+        public void AssignToShelve(Shelve shelve)
+        {
+            shelve.AddBook(this);
         }
     }
 }
